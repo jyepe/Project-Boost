6d6d5f77f8c3f599b4ace929b126d85b881bf3fd
jyepe <yepejy@gmail.com>
2018-03-14 00:44:15 -0400

    Camera Moves
    Added camera movement

---
+++ Assets/Scripts/MoveCamera.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveCamera : MonoBehaviour {
+
+    [SerializeField] Vector3 movementVector;
+    float movementFactor;
+    [SerializeField] [Range(0,1)] float movementSpeed;
+    Vector3 startingPosition;
+
+    // Use this for initialization
+    void Start () {
+        startingPosition = transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        cameraMovement();
+	}
+
+    private void cameraMovement()
+    {
+        movementFactor += movementSpeed;
+        Vector3 offset = movementVector * movementFactor;
+        transform.position = offset + startingPosition;
+    }
+}
